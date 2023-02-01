e5ba83065a03515890b0bf95e124871a4a80a9b0
Jose Goulart <jose.goulart@acad.pucrs.br>
2023-02-01 14:01:40 -0300

    check buttons

--- components/ServicesScreen.js
+++ components/ServicesScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Modal, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   plusButton: {
     position: 'absolute',
     bottom: 20,
-    right: 20,
+    left: 120,
     width: 50,
     height: 50,
     borderRadius: 25,
@@ -44,8 +44,34 @@ const styles = StyleSheet.create({
   preview: {
     alignSelf: 'stretch',
     flex: 1
+  },
+  buttonContainer: {
+    marginTop: 40,
+    position: 'relative',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    paddingHorizontal: 20
+  },
+  filterIconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  filterText: {
+    color: 'white',
+    fontSize: 16,
+    marginRight: 10
   }
 });
+
+const correctivesChecks = ["Foto comparando o solo", "Foto do serviço sendo executado", "Foto do termômetro de temperatura",
+  "Foto do relógio de pressão (se arrumar a porta externa", "Foto do OS da prefeitura e da empresa assinada (caso tenha)", "Motivo e foto caso serviço não foi realizado"];
+const preventivesChecks = ["Foto comparando o solo", "Foto do serviço sendo executado", "Foto do termômetro de temperatura",
+  "Foto do relógio de pressão (se arrumar a porta externa", "Foto do aparelho limpo", "Motivo e foto caso serviço não foi realizado"];
 
 function getAllRooms(employees) {
   let rooms = [];
@@ -62,6 +88,31 @@ function ServicesScreen({ route }) {
   const [services, setServices] = useState([]);
   const { room } = route.params;
   const navigation = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible2, setModalVisible2] = useState(false);
+
+  function OpenModal({ checks, modalVisible, setModalVisible }) {
+    return (
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}>
+        <View style={{ backgroundColor: 'white', width: 300, height: 400, padding: 20, borderRadius: 10 }}>
+            <FlatList
+              data={checks}
+              renderItem={({ item }) => (
+                <Text>- {item}</Text>
+              )}
+            ></FlatList>
+            <Button title="Fechar" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+    );
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -88,6 +139,24 @@ function ServicesScreen({ route }) {
             </View>
           )}
         />
+        <TouchableOpacity
+          style={[styles.buttonContainer, { backgroundColor: '#0077C9', borderRadius: 10 }]}
+          onPress={() => { setModalVisible(true) }}
+        >
+          <View style={styles.filterIconContainer}>
+            <Text style={styles.filterText}>Ver checagens corretivas</Text>
+          </View>
+        </TouchableOpacity>
+        {modalVisible && <OpenModal checks={correctivesChecks} modalVisible={modalVisible} setModalVisible={setModalVisible} />}
+        <TouchableOpacity
+          style={[styles.buttonContainer, { backgroundColor: '#0077C9', borderRadius: 10 }]}
+          onPress={() => {setModalVisible2(true)}}
+        >
+          <View style={styles.filterIconContainer}>
+            <Text style={styles.filterText}>Ver checagens preventivas</Text>
+          </View>
+        </TouchableOpacity>
+        {modalVisible2 && <OpenModal checks={preventivesChecks} modalVisible={modalVisible2} setModalVisible={setModalVisible2} />}
       </View>
       <View style={styles.container}>
         <TouchableOpacity
